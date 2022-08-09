92c090389d7917a39f1b9f89027cd99fb683b2dc
hanthienduc <hanthienduc.96@gmail.com>
2022-08-10 07:54:35 +0900

    add temp userId and add get posts route to get all the posts

--- server/server.js
+++ server/server.js
@@ -16,3 +16,29 @@ app.register(fastifyCors, {
   credentials: true
 })
 
+const prisma = new PrismaClient()
+
+const CURRENT_USER_ID = (await prisma.user.findFirst({ where: { name: 'Sally' } })).id
+
+app.addHook("onRequest", (req, res, done) => {
+  if (req.cookies.userId !== CURRENT_USER_ID) {
+    req.cookies.userId = CURRENT_USER_ID
+    req.clearCookie("userId")
+    req.setCookie("userId", CURRENT_USER_ID)
+  }
+})
+
+app.get('/posts', (req, res) => {
+  return await commitToDb(prisma.post.findMany({
+    select: {
+      id: true,
+      title: true
+    }
+  }))
+})
+
+async function commitToDb(promise) {
+  const [error, data] = await app.to(promise)
+  if (error) return app.httpErrors.internalServerError(error.message)
+  return data
+}
