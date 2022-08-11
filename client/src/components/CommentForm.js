9691ae07b99d7dd173207039f66eab17276aad73
hanthienduc <hanthienduc.96@gmail.com>
2022-08-11 18:15:24 +0900

    add comment form component

---
+++ client/src/components/CommentForm.js
@@ -0,0 +1,25 @@
+import { useState } from "react"
+
+export function CommentForm({ loading, error, onSubmit, autoFocus = false, initialValue = '' }) {
+
+  const [message, setMessage] = useState(initialValue)
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    onSubmit(message).then(() => setMessage(''))
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="comment-form-row">
+        <textarea value={message} className="form-submit" autoFocus={autoFocus}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button disabled={loading} className="btn">{loading ? 'Loading... ' : 'Post'}</button>
+      </div>
+      <div className="error-msg">
+        {error}
+      </div>
+    </form>
+  )
+}
--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,4 +1,5 @@
 import { usePostContext } from "../context/PostContext"
+import { CommentForm } from "./CommentForm"
 import { CommentList } from "./CommentList"
 
 export function Post() {
@@ -8,7 +9,9 @@ export function Post() {
   return <>
     <h1>{post.title}</h1>
     <article>{post.body}</article>
+    <h3 className="comments-title">Comments</h3>
     <section>
+      <CommentForm autoFocus />
       {rootComments != null && rootComments.length > 0 && (
         <div className="mt-4">
           <CommentList comments={rootComments} />
